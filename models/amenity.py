6ba36aa12517d5f93042c33cc24e0e36133962c6
johnechono <johninalegwu22@gmail.com>
2023-09-20 19:09:26 +0000

    amenities

--- models/amenity.py
+++ models/amenity.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""Defines the Amenity class."""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import Column, String
+from models.place import place_amenity
 from sqlalchemy.orm import relationship
+from uuid import uuid4
 
 
 class Amenity(BaseModel, Base):
-    """Represents an Amenity for a MySQL database.
+    ''' defines Amenity class '''
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary=place_amenity)
 
-    Inherits from SQLAlchemy Base and links to MySQL table amenities.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Amenities.
-        name (sqlalchemy String): The amenity name.
-        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+        def __init__(self, **kwargs):
+            self.id = str(uuid4())
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+    else:
+        name = ''
